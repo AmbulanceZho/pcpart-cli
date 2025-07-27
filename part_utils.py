99ce29c1258bbde0868903b5bf32420e9b1735e7
AmbulanceZho <gliam7354@gmail.com>
2025-07-27 03:27:53 -0400

    Add files via upload

--- part_utils.py
+++ part_utils.py
@@ -7,11 +7,20 @@ cpu_specs = ["brand", "name", "clock", "boost_clock", "core_count", "thread_coun
 def cpu():
     
     with database_connection(get_database_path()) as database:
+        cpuspecs = {}
         for spec in cpu_specs:
             get_spec = input(f"Enter spec for {spec}: ")
-            pass
-            
+            cpuspecs[spec] = get_spec
         
+        columns = ", ".join(cpuspecs.keys())
+        place_holders = ", ".join("?" for _ in cpuspecs)
+        values = tuple(cpuspecs.values())
+        
+        sql = f"INSERT INTO cpu ({columns}) VALUES ({place_holders})"
+        
+        database_cursor = database.cursor()
+        database_cursor.execute(sql, values)
+        database.commit()     
     
 def gpu():
     pass
